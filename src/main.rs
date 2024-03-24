f130deaa4f7b6d1dfa8528a848a2eb67a6810f8b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-03-24 15:34:57 +0530

    do not enforce a size requirement, the server can be reused for other keys as well

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::convert::Infallible;
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
 use std::net::SocketAddr;
 use std::str::FromStr;
 
@@ -26,15 +24,15 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
-    let mut file = File::open(cli.pub_key)?;
-    let mut pub_key = [0; 32];
-    file.read_exact(&mut pub_key)?;
+    // leak in order to get a static slice
+    // okay to do since it will get cleaned up on exit
+    let pub_key = std::fs::read(cli.pub_key)?.leak::<'static>();
     println!("pub key: {:02x?}", pub_key);
 
     let addr = SocketAddr::from_str(&cli.ip_addr)?;
 
-    let make_svc = make_service_fn(move |_conn| {
-        let service = service_fn(move |_req| async move {
+    let make_svc = make_service_fn(|_conn| {
+        let service = service_fn(|_req| async {
             Ok::<_, Infallible>(Response::<Body>::new(
                 oyster_attestation_server_ed25519::get_attestation_doc(pub_key).into(),
             ))
