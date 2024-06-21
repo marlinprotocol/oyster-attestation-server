c73689e828f919ec5b3bda456ef46ab7d5df47af
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-06-21 15:14:45 +0530

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 use aws_nitro_enclaves_nsm_api::api::{Request, Response};
 use aws_nitro_enclaves_nsm_api::driver as nsm_driver;
-use serde::Serialize;
 use serde_bytes::ByteBuf;
-
-#[derive(Serialize)]
-struct EnclaveConfig {
-    total_memory: u64,
-    total_cpus: usize,
-}
 
 pub fn get_attestation_doc(pub_key: &[u8]) -> Vec<u8> {
     let public_key = ByteBuf::from(pub_key);
